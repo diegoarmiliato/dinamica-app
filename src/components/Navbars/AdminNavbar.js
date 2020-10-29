5e108d358b7e710665c729833509905a8938f9b4
Diego Armiliato <diegoarmiliato@gmail.com>
2020-10-28 21:14:41 -0300

    added Dinamica logo and logoff function

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -18,10 +18,11 @@ import {
   Media
 } from "reactstrap";
 import { Context } from "store";
+import { loginActions } from "store/reducers/login";
 
 function AdminNavbar(props) {
 
-  const { state } = useContext(Context);
+  const { state, dispatch } = useContext(Context);
 
   const { login } = state;
 
@@ -69,29 +70,9 @@ function AdminNavbar(props) {
                 </Media>
               </DropdownToggle>
               <DropdownMenu className="dropdown-menu-arrow" right>
-                <DropdownItem className="noti-title" header tag="div">
-                  <h6 className="text-overflow m-0">Welcome!</h6>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-single-02" />
-                  <span>My profile</span>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-settings-gear-65" />
-                  <span>Settings</span>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-calendar-grid-58" />
-                  <span>Activity</span>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-support-16" />
-                  <span>Support</span>
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
+                <DropdownItem href="#pablo" onClick={() => dispatch({ type: loginActions.LOGOFF })}>
                   <i className="ni ni-user-run" />
-                  <span>Logout</span>
+                  <span>Logoff</span>
                 </DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
